c820eda2841f856572dbcc90a36b6ce898cb5413
Dhvani Gohel <dhvanigohel03@gmail.com>
2025-05-27 14:38:13 +0530

    ABC Model Trained TensorFlow

--- venv/dataCollection.py
+++ venv/dataCollection.py
@@ -2,12 +2,22 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np 
 import math 
+import time
+
+
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1) #Maximum Amount of hands to be detected
 
 offset = 20 #so the cropped image isnt tightbound, has some extra space
 imgSize = 300 #defined image size
+
+
+folder = "Data/C"
+counter = 0
+
+
+
 
 #Starts the Camera
 while True:
@@ -26,25 +36,35 @@ while True:
         imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255 #a square of 300x300, 3 is the colur information a white block
 
         aspectRatio = h / w
+        paddingRatio = 0.9  # Scale down final image to leave margin (10% margin inside the white square)
+
 
         if aspectRatio > 1:
-            k = imgSize / h
+            k = (imgSize * paddingRatio) / h  # scale down to leave margin
             wCal = math.ceil(k * w)
-            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResize = cv2.resize(imgCrop, (wCal, int(imgSize * paddingRatio)))
             imgResizeShape = imgResize.shape
             wGap = math.ceil((imgSize - wCal) / 2)
-            imgWhite[:, wGap:wCal + wGap] = imgResize
+            hGap = math.ceil((imgSize - imgResizeShape[0]) / 2)
+            imgWhite[hGap:hGap + imgResizeShape[0], wGap:wGap + wCal] = imgResize
 
         else:
-            k = imgSize / w
+            k = (imgSize * paddingRatio) / w
             hCal = math.ceil(k * h)
-            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResize = cv2.resize(imgCrop, (int(imgSize * paddingRatio), hCal))
             imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize - imgResizeShape[1]) / 2)
             hGap = math.ceil((imgSize - hCal) / 2)
-            imgWhite[hGap:hCal + hGap, :] = imgResize
+            imgWhite[hGap:hGap + hCal, wGap:wGap + imgResizeShape[1]] = imgResize
+
 
         cv2.imshow("ImageCrop", imgCrop)
         cv2.imshow("ImageWhite", imgWhite)
 
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    if key == ord("s"):
+        counter += 1
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
+        print(counter)
+
