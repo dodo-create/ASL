15fe7145430be2526b0a47db076f1a239824a135
Dhvani Gohel <dhvanigohel03@gmail.com>
2025-05-27 13:23:28 +0530

    Height = 300

--- venv/dataCollection.py
+++ venv/dataCollection.py
@@ -1,6 +1,7 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np 
+import math 
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1) #Maximum Amount of hands to be detected
@@ -25,7 +26,19 @@ while True:
         imgWhite = np.ones((imgSize,imgSize,3),np.uint8)*255 #a square of 300x300, 3 is the colur information a white block
         imgCropShape = imgCrop.shape #matrix of 3 values height,width,channel
 
-        imgWhite[0:imgCropShape[0],0:imgCropShape[1]] = imgCrop #Opens the image on top of the white image
+       
+
+        aspectRatio = h/w #if value >1 height is greater, if value<1 width is greater if value=1 its a sqaure
+
+        if aspectRatio > 1:
+            k = imgSize/h  #stretching the height
+            wCal = math.ceil(k*w) #rounds off to the higher integer
+            imgResize = cv2.resize(imgCrop,(wCal,imgSize))
+            imgResizeShape = imgResize.shape
+            imgWhite[0:imgResizeShape[0],0:imgResizeShape[1]] = imgCrop #Opens the image on top of the white image
+            
+
+
 
 
         imgCrop = img[y-offset:y+h+offset,x-offset:x+w+offset] #since it is a matrix, we have defined the ranges/dimensions of the crop 
